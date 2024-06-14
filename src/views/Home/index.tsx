a02b4e5d5873f32b1d26c59a35908dcf27145899
devjohn42 <devjohncb@gmail.com>
2024-06-14 19:11:54 -0300

    feat: creates the functionality to get device contacts

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Header, HeaderTitle } from './styles';
 import Researcher from './components/Researcher';
 import TextInputContainer from './components/TextInputField';
 import { theme } from '@/global/theme';
 import { Feather } from '@expo/vector-icons';
+import * as Contacts from 'expo-contacts';
 import Contact from './components/Contact';
+import { Alert } from 'react-native';
 
 export default function Home() {
   const [name, setName] = useState('');
+
+  const fetchContacts = async () => {
+    try {
+      const { status } = await Contacts.requestPermissionsAsync();
+
+      if (status === Contacts.PermissionStatus.GRANTED) {
+        const { data } = await Contacts.getContactsAsync();
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Contatos', 'Não foi possível acessar os contatos');
+    }
+  };
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
   return (
     <Container>
       <Header>
